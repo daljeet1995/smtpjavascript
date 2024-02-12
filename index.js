6fe7c5c78c3d679c67165bcc2b3b1bdc4ebd5f72
unknown <ds025272@gmail.com>
2024-02-12 14:41:09 +0530

    Secure Password Generator Online - Strong, random

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ submit.addEventListener('submit',(e)=>{
     `;
 
     Email.send({
-        SecureToken : "3e4e91c7-235d-4631-a42f-29dc75e51e63", //add your token here
+        SecureToken : "41373d94-3577-44ad-adb7-06e09a8d9ee0", //add your token here
         To : 'ds025272@gmail.com', 
         From : "ds025272@gmail.com",
         Subject : "This is the subject",
